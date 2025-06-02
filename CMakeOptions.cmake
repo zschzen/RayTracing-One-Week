d08058a36904a1043b5dea3d54ad47fcfe5f7420
Leandro Peres <leandroperes@protonmail.com>
2025-06-02 12:56:57 -0300

    build: Add cmake files

---
+++ CMakeOptions.cmake
@@ -0,0 +1,6 @@
+include(CMakeDependentOption)
+
+#--------------------------------------------------------------------
+# Build Options
+#--------------------------------------------------------------------
+option(USE_CCACHE "Enable compiler cache that can improve build times" ${IS_MAIN})
